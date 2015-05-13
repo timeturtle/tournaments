4568a728181353d6a2988b352e563b8ccc292274
patrickedqvist <patrick.edqvist@gmail.com>
2015-05-13 19:52:46 +0200

    Workplace arranged
    Added a svg, set theme colors and made a navigation toggle. Also set up
    a layout to work with.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,19 @@
 var app = angular.module('TournamentApp', ['ngMaterial']);
 
+app.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+  .primaryPalette('green')
+  .accentPalette('orange');
+});
+
 app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-  $scope.toggleSidenav = function(menuId) {
-    $mdSidenav(menuId).toggle();
+
+  $scope.toggleNav = function () {
+    $mdSidenav('left').toggle();
+  };
+
+  $scope.close = function () {
+    $mdSidenav('left').close();
   };
 
 }]);
