dfc3a1b907faf7efee96806aa9845967bbcc10a6
patrickedqvist <patrick.edqvist@gmail.com>
2015-05-13 19:11:20 +0200

    Refurnish

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+var app = angular.module('TournamentApp', ['ngMaterial']);
+
+app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+
+}]);
